8a74657ce36a1bfa626a987e45e3f1f2929e6bb4
Jack Meng <jackmeng0814@gmail.com>
2023-03-29 18:36:50 -0400

    `add` i will work on this stupid thing later  :skull:

--- pkg/js/src/app.js
+++ pkg/js/src/app.js
@@ -2,3 +2,40 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+const {
+  Client,
+  GatewayIntentBits,
+  Collection,
+  IntentsBitField,
+  cleanContent,
+} = require("discord.js");
+
+const config = require("../../config/bot.json");
+const internal = require("../../../ipkg/config/h.json");
+
+const express = require("express");
+const app = express();
+const port = config.local["server-port"];
+
+const bot = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+app.get("/", (r, res) => res.send("The bot is online! :DD"));
+app.listen(port, () => console.log("Alive on port: " + port));
+
+config.commands.prefixing.forEach((x) => (bot[x] = new Collection()));
+
+bot.setMaxListeners(15);
+bot.once("ready", () => {
+  console.log("API is ready");
+  console.log(
+    "Entered as: " + bot.user?.username + "#" + bot.user?.discriminator
+  );
+});
+
+bot.login(internal["BOT-TOKEN"]);
